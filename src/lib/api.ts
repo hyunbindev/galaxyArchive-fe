14222fb6a87c18872f67b1fcc4b16647e85d184e
김현빈 <hyunbin@carrotui-MacBookAir.local>
2026-04-23 01:27:08 +0900

    update flow, docker

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.INTERNAL_API_URL || 'http://localhost:8080'}`,
     timeout: 5000,
     withCredentials: true,
 });
