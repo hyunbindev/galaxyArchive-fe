dca567d3b2e780462c7b6087ed342d3ca8f6e3b0
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-06-03 22:15:44 +0900

    fix comment bug about userinfo props

--- src/app/(main)/article/[id]/component/comment/CommentField.tsx
+++ src/app/(main)/article/[id]/component/comment/CommentField.tsx
@@ -18,16 +18,14 @@ import {useRouter} from "next/navigation";
 
 interface CommentFieldProps {
     articleId:number;
-    userInfo:UserInfo;
+    userInfo:UserInfo|null;
 }
 
 export default function CommentField({articleId, userInfo}:CommentFieldProps){
-    const router = useRouter();
 
     const { text , setText , requestArticleComment } = useCreateComment(articleId);
 
     const placeHolder:string = userInfo? "write a comment..." : "Log in to leave a comment"
-
 
     return(
         <div className="relative ml-10 pb-10">
@@ -42,7 +40,7 @@ export default function CommentField({articleId, userInfo}:CommentFieldProps){
                             onChange={e=>{ setText(e.target.value)} }
                             placeholder={placeHolder}
                         />
-                        <Button variant="default" size="xs" className={cn("absolute right-2 bottom-1.5 cursor-pointer")} onClick={requestArticleComment}>
+                        <Button disabled={!userInfo} variant="default" size="xs" className={cn("absolute right-2 bottom-1.5 cursor-pointer")} onClick={requestArticleComment}>
                             <SendHorizontal/>
                         </Button>
                     </InputGroup>
