38e546e47610a36a894add5406183a272f48c348
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-06-10 23:33:00 +0900

    fix article card layout

--- src/app/(main)/user/[id]/article/ArticleSummaryElement.tsx
+++ src/app/(main)/user/[id]/article/ArticleSummaryElement.tsx
@@ -11,9 +11,11 @@ export default function ArticleSummaryElement({articleSummary}:ArticleSummaryEle
 
     return(
         <Link href={`/article/${articleSummary.id}`}>
-            <div className="h-60 flex flex-col justify-between border rounded-sm px-3 py-6 hover:bg-zinc-100 dark:hover:bg-zinc-900 cursor-pointer">
-                <span className="text-xs text-muted-foreground">{dateConvert(articleSummary.createdAt)}</span>
-                <h3 className="mb-3">{articleSummary.title}</h3>
+            <div className="h-56 flex flex-col justify-between border rounded-sm px-3 py-6 hover:bg-zinc-100 dark:hover:bg-zinc-900 cursor-pointer">
+                <div className="flex flex-col">
+                    <span className="text-xs top-2 text-muted-foreground">{dateConvert(articleSummary.createdAt)}</span>
+                    <h3 className="mb-3 line-clamp-2">{articleSummary.title}</h3>
+                </div>
                 <span className="text-sm text-muted-foreground mb-3 line-clamp-3">{articleSummary.description}</span>
                 <span className="text-xs text-muted-foreground">{articleSummary.commentsCount} comments</span>
             </div>
